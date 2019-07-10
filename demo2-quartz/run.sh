7efe19d44bc2f79713d48f58ba3dcabaee983372
quuee <33923687+quuee@users.noreply.github.com>
2019-07-10 17:25:41 +0800

    Update run.sh

--- demo2-quartz/run.sh
+++ demo2-quartz/run.sh
@@ -4,7 +4,7 @@
 docker build -t quartz:1.0 -f Dockerfile .
 
 # 创建启动容器
-docker run -d --name quartz -m 500m -p 23333:2333 \
+docker run -d --name quartz -m 500m -p 23333:23333 \
 --log-driver json-file \
 --log-opt max-size=10m \
 --log-opt max-file=3 \
